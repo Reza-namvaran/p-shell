bdca524e4f8797493796191d031e69c5c462c463
Reza-namvaran <reza.namvaran823@gmail.com>
2025-09-23 02:12:36 +0330

    (feat): Add I/O redirects
    Add stream (< and >) redirectors for command execution

--- src/main.c
+++ src/main.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <fcntl.h>
 
 #define TOKEN_DELIMITERS " \t\r\n\a"
 #define PROMPT "\u03c0> "
@@ -10,8 +11,9 @@
 char **split_lines(char *line);
 int psh_cd(char **args);
 int psh_exit(char **args);
+int psh_execute_and_redirect(char **args);
 int psh_execute(char **args);
-int psh_lunch(char **args);
+int psh_lunch(char **args, char *infile, char *outfile, int append);
 int psh_help(char **args);
 void shell_loop(void);
 
@@ -88,7 +90,7 @@ char **split_lines(char *line) {
     return tokens;
 }
 
-int psh_lunch(char **args) {
+int psh_lunch(char **args, char *infile, char *outfile, int append) {
     pid_t pid;
     int status;
 
@@ -102,6 +104,33 @@ int psh_lunch(char **args) {
     // Create child process
     pid = fork();
     if (pid == 0) {
+        // child process
+
+        if (infile) {
+            int fd_in = open(infile, O_RDONLY);
+            if (fd_in < 0) {
+                perror("psh in-file");
+                exit(EXIT_FAILURE);
+            }
+        
+            dup2(fd_in, STDIN_FILENO);
+            close(fd_in);
+        }
+
+        if (outfile) {
+            int flags = O_WRONLY | O_CREAT | (append ? O_APPEND : O_TRUNC);
+            int fd_out = open(outfile, flags, 0644);
+            if (fd_out < 0) {
+                perror("psh: fd_out");
+                exit(EXIT_FAILURE);
+            }
+
+            dup2(fd_out, STDOUT_FILENO);
+            close(fd_out);
+        }
+
+
+
         if (execvp(args[0], args) == -1) {
             perror("Error occurred in forking");
         }
@@ -116,6 +145,61 @@ int psh_lunch(char **args) {
     }
 
     return 1;
+}
+
+int psh_execute_and_redirect(char **args) {
+    char *infile = NULL;
+    char *outfile = NULL;
+    int append = 0;
+
+    // Create a new array for command and arguments only
+    char **exec_args = malloc(sizeof(char*) * 64);
+    if (!exec_args) {
+        perror("allocation error");
+        exit(EXIT_FAILURE);
+    }
+    int exec_pos = 0;
+
+    /// TODO: Make this faster
+    for (int i = 0; args[i] != NULL; i++) {
+        if (strcmp(args[i], ">") == 0) {
+            if (args[i + 1] == NULL) {
+                fprintf(stderr, "psh: syntax error: missing filename for redirection\n");
+                free(exec_args);
+                return 1;
+            }
+            outfile = args[i + 1];
+            i++;
+            continue;
+        }
+    
+        if (strcmp(args[i], "<") == 0) {
+            if (args[i+1] == NULL) {
+                fprintf(stderr, "psh: syntax error: missing filename for redirection\n");
+                free(exec_args);
+                return 1;
+            }
+
+            infile = args[i + 1];
+            i++;
+            continue;
+        }
+
+        exec_args[exec_pos++] = args[i];
+    }
+    exec_args[exec_pos] = NULL;
+
+    // If there are no arguments for the command, return
+    if (exec_args[0] == NULL) {
+      free(exec_args);
+      return 1;
+    }
+
+    // Launch the command with the parsed redirection options
+    int status = psh_lunch(exec_args, infile, outfile, append);
+
+    free(exec_args);
+    return status;
 }
 
 int psh_execute(char **args) {
@@ -133,14 +217,14 @@ int psh_execute(char **args) {
     }
 
     // If not builtin lunch in a separate process
-    return psh_lunch(args);
+    return psh_execute_and_redirect(args);
 }
 
 void shell_loop(void) {
     char *line = NULL;
     size_t buff_size = 0;
     char **args;
-    int status;
+    int status = 1;
 
     while (1) {
         printf(PROMPT);
